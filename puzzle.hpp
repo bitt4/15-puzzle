d40e1221969e370eedcdd916999d8a939a9ca5fb
bitt4 <jakub.bittara@gmail.com>
2020-08-29 22:45:33 +0200

    renderValue() to render numbers

--- puzzle.hpp
+++ puzzle.hpp
@@ -1,9 +1,14 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <string>
 
 class puzzle{
 private:
     SDL_Renderer *renderer;
+    TTF_Font* defaultFont;
+    SDL_Color fontColor;
 public:
     puzzle(SDL_Renderer* renderer);
     void renderInit();
+    void renderValue();
 };
