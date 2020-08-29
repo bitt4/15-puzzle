65eb28a2ec6716b930e048f0331331d863d7691d
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-08-29 22:45:57 +0200

    Implement renderValue() function But I want to change font loading, and it looks very ugly right now

--- puzzle.cpp
+++ puzzle.cpp
@@ -3,7 +3,8 @@
 puzzle::puzzle(SDL_Renderer *renderer)
     :renderer(renderer)
 {
-
+    this->defaultFont = TTF_OpenFont("Arial.ttf", 96);
+    this->fontColor = {255, 255, 255, 255};
 }
 
 void puzzle::renderInit(){
@@ -27,5 +28,32 @@ void puzzle::renderInit(){
                            150 * i + i);
     }
 
+    renderValue();
     SDL_RenderPresent(renderer);
+}
+
+void puzzle::renderValue(){
+    SDL_Surface* cellText;
+    SDL_Texture* cellTexture;
+
+    for(int y = 0; y < 4; y++){
+        for(int x = 0; x < 4; x++){
+            cellText = TTF_RenderText_Solid(this->defaultFont,
+                                            std::to_string(y * 4 + x).c_str(),
+                                            this->fontColor);
+
+            cellTexture = SDL_CreateTextureFromSurface(this->renderer, cellText);
+
+            SDL_Rect cellRectangle;
+            cellRectangle.x = x * 150 + x;
+            cellRectangle.y = y * 150 + y;
+            cellRectangle.w = 150;
+            cellRectangle.h = 150;
+
+            SDL_RenderCopy(this->renderer, cellTexture, NULL, &cellRectangle);
+        }
+    }
+
+    SDL_FreeSurface(cellText);
+    SDL_DestroyTexture(cellTexture);
 }
