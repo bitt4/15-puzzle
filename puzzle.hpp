7d9656dcb9e486f36ca72d9ed40f49cecf0cf298
bitt4 <jakub.bittara@gmail.com>
2020-09-02 20:43:51 +0200

    quit(), isGameOver() and getEndGame() functions

--- puzzle.hpp
+++ puzzle.hpp
@@ -9,13 +9,17 @@ private:
     TTF_Font* defaultFont;
     SDL_Color fontColor;
     int *tiles;
+    bool endGame;
 
     /* Swap target tile with the empty one */
     void swapTiles(int target_x, int target_y);
+    void renderValue();
+    bool isGameOver();
 public:
     puzzle(SDL_Renderer* renderer);
     ~puzzle();
     void render();
-    void renderValue();
     void click(int x, int y);
+    bool getEndGame();
+    void quit();
 };
