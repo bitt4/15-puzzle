e0cd74715c84ea10e44895cadcb7f37d7be16112
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-09-02 17:37:59 +0200

    Tiles

--- puzzle.hpp
+++ puzzle.hpp
@@ -1,12 +1,14 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 #include <string>
+#include <cstdlib>
 
 class puzzle{
 private:
     SDL_Renderer *renderer;
     TTF_Font* defaultFont;
     SDL_Color fontColor;
+    int *tiles;
 public:
     puzzle(SDL_Renderer* renderer);
     void renderInit();
