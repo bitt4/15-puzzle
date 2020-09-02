2760847c775a741e609ae94adf7242e9f0b72626
bitt4 <jakub.bittara@gmail.com>
2020-09-02 18:09:06 +0200

    swapTiles() and click(int, int) functions Renamed renderInit() -> render()

--- puzzle.hpp
+++ puzzle.hpp
@@ -9,9 +9,13 @@ private:
     TTF_Font* defaultFont;
     SDL_Color fontColor;
     int *tiles;
+
+    /* Swap target tile with the empty one */
+    void swapTiles(int target_x, int target_y);
 public:
     puzzle(SDL_Renderer* renderer);
     ~puzzle();
-    void renderInit();
+    void render();
     void renderValue();
+    void click(int x, int y);
 };
