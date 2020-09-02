f3344cf1aaee7d82d7a7df37ad5f26539f9815cd
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-09-02 18:09:53 +0200

    Implement swapTiles() and click() functions You can swap tiles that are not next to each other (TODO in swapTiles()) Don't render tile with value 0 (empty)

--- puzzle.cpp
+++ puzzle.cpp
@@ -18,7 +18,7 @@ puzzle::~puzzle(){
     free(this->tiles);
 }
 
-void puzzle::renderInit(){
+void puzzle::render(){
     /* Render black empty board */
     SDL_SetRenderDrawColor(this->renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
@@ -49,29 +49,52 @@ void puzzle::renderValue(){
 
     for(int y = 0; y < 4; y++){
         for(int x = 0; x < 4; x++){
-            int textWidth, textHeight;
+            if(this->tiles[y * 4 + x]){
+                int textWidth, textHeight;
 
-            TTF_SizeText(this->defaultFont,
-                         std::to_string(this->tiles[y * 4 + x]).c_str(),
-                         &textWidth,
-                         &textHeight);
+                TTF_SizeText(this->defaultFont,
+                             std::to_string(this->tiles[y * 4 + x]).c_str(),
+                             &textWidth,
+                             &textHeight);
 
-            cellText = TTF_RenderText_Blended(this->defaultFont,
-                                              std::to_string(this->tiles[y * 4 + x]).c_str(),
-                                              this->fontColor);
+                cellText = TTF_RenderText_Blended(this->defaultFont,
+                                                  std::to_string(this->tiles[y * 4 + x]).c_str(),
+                                                  this->fontColor);
 
-            cellTexture = SDL_CreateTextureFromSurface(this->renderer, cellText);
+                cellTexture = SDL_CreateTextureFromSurface(this->renderer, cellText);
 
-            SDL_Rect cellRectangle;
-            cellRectangle.x = x * 150 + x + (150-textWidth)/2;
-            cellRectangle.y = y * 150 + y + (150-textHeight)/2;
-            cellRectangle.w = textWidth;
-            cellRectangle.h = textHeight;
+                SDL_Rect cellRectangle;
+                cellRectangle.x = x * 150 + x + (150-textWidth)/2;
+                cellRectangle.y = y * 150 + y + (150-textHeight)/2;
+                cellRectangle.w = textWidth;
+                cellRectangle.h = textHeight;
 
-            SDL_RenderCopy(this->renderer, cellTexture, NULL, &cellRectangle);
+                SDL_RenderCopy(this->renderer, cellTexture, NULL, &cellRectangle);
+            }
         }
     }
 
     SDL_FreeSurface(cellText);
     SDL_DestroyTexture(cellTexture);
+}
+
+void puzzle::swapTiles(int x, int y){
+    /* TODO: check if target tile is next to empty tile */
+
+    /* Search for empty tile */
+    int emptyTilePosition;
+    for(int i = 0; i < 16; i++){
+        if(!this->tiles[i]){
+            emptyTilePosition = i;
+        }
+    }
+
+    int temp = this->tiles[y * 4 + x];
+    this->tiles[y * 4 + x] = 0;
+    this->tiles[emptyTilePosition] = temp;
+}
+
+void puzzle::click(int x, int y){
+    swapTiles(x, y);
+    render();
 }
