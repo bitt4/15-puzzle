d29f69ee0374fe74a58925a2565a6b8ef8376ca9
bitt4 <jakub.bittara@gmail.com>
2020-09-22 17:48:22 +0200

    Documented some functions

--- puzzle.hpp
+++ puzzle.hpp
@@ -20,10 +20,37 @@ private:
 
     /* Swap target tile with the empty one */
     void swapTiles(int target_x, int target_y);
+
+    /*
+     * Renders numbers in tiles by `fontColor`,
+     * which is white by default.
+     * To render numbers by arbitrary color,
+     * use puzzle::renderValue(SDL_Color color)
+     */
     void renderValue();
+
+    /*
+     * Renders numbers in tiles by arbitrary
+     * color specified by `color` argument
+     */
     void renderValue(SDL_Color color);
+
+    /*
+     * Returns true, if the puzzle is completed,
+     * otherwise returns false
+     */
     bool isGameOver();
+
+    /*
+     * Returns true, if current array `tiles`
+     * contains solvable configuration of tiles,
+     * source: www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
+     */
     bool isSolvable();
+
+    /*
+     * Shuffles `tiles` array (swaps random tiles)
+     */
     void shuffle();
     void restart();
     Point getEmptyTile();
