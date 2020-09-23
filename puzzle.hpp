b5a10c10eb03e8f9c0684609e7015a5e7cfe317c
bitt4 <jakub.bittara@gmail.com>
2020-09-23 21:39:15 +0200

    Documented some functions and members

--- puzzle.hpp
+++ puzzle.hpp
@@ -13,10 +13,38 @@ typedef struct {
 class puzzle{
 private:
     SDL_Renderer *renderer;
+
+    /* Font by which numbers are rendered */
     TTF_Font* defaultFont;
+
+    /* Colors for rendering numbers in tiles.
+     * fontColor: defines default font color of numbers in tiles,
+     *            used in puzzle::renderValue(SDL_Color)
+     *            as default value
+     * winColor: used as argument in puzzle::render(SDL_Color),
+     *           when the puzzle is completed
+     */
     SDL_Color fontColor, winColor;
+
+    /*
+     * Contains tiles
+     */
     int *tiles;
-    bool endGame, gameOver;
+
+    /*
+     * Used to stop the game loop.
+     * When user closes the window, `endGame` is set
+     * to true, terminating the game loop.
+     * Returned by puzzle::getEndGame() (getter).
+     */
+    bool endGame;
+
+    /*
+     * Specifies state of puzzle.
+     * True, if the puzzle is completed, otherwise false.
+     * Used to restart the puzzle.
+     */
+    bool gameOver;
 
     /* Swap target tile with the empty one */
     void swapTiles(int target_x, int target_y);
@@ -52,7 +80,20 @@ private:
      * Shuffles `tiles` array (swaps random tiles)
      */
     void shuffle();
+
+    /*
+     * Restarts state of game:
+     * sets `gameOver` and `endGame` to false,
+     * shuffles tiles with puzzle::shuffle(),
+     * and calls puzzle::render() to render game
+     */
     void restart();
+
+    /*
+     * Searches for empty tile. Returns `Point`
+     * structure, which contains x (column)
+     * and y (row) array coordinates in `tiles` array
+     */
     Point getEmptyTile();
 public:
     puzzle(SDL_Renderer* renderer);
