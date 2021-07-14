42898cba604915570792d9196fe0cc825966caff
bitt4 <jakub.bittara@gmail.com>
2021-07-14 13:28:04 +0200

    Remove unnecessary comments

--- src/puzzle.hpp
+++ src/puzzle.hpp
@@ -15,131 +15,33 @@ typedef struct {
 class Puzzle {
 private:
     SDL_Renderer *renderer;
-
-    /* Font by which numbers are rendered */
     TTF_Font* font;
-
-    /* Colors for rendering numbers in tiles.
-     * fontColor: defines default font color of numbers in tiles,
-     *            used in puzzle::renderValue(SDL_Color)
-     *            as default value
-     * winColor: used as argument in puzzle::render(SDL_Color),
-     *           when the puzzle is completed
-     */
     SDL_Color fontColor, winColor;
-
-    /*
-     * Contains tiles
-     */
     int *tiles;
-
-    /*
-     * Used to stop the game loop.
-     * When user closes the window, `endGame` is set
-     * to true, terminating the game loop.
-     * Returned by puzzle::getEndGame() (getter).
-     */
     bool endGame;
-
-    /*
-     * Specifies state of puzzle.
-     * True, if the puzzle is completed, otherwise false.
-     * Used to restart the puzzle.
-     */
     bool gameOver;
-
-    /* Stores path of a directory from which has the executable been
-     * run
-     */
     const char* basePath;
-
-    /* Stores the full path of last file passed to function
-     * puzzle::getPath(const char* filename), basically
-     * `basePath`+`filename`
-     */
     char* currentFilePath;
-
-    /* Swap target tile with the empty one */
     void swapTiles(int target_x, int target_y);
-
-    /*
-     * Renders numbers in tiles by `fontColor`,
-     * which is white by default.
-     * To render numbers by arbitrary color,
-     * use puzzle::renderValue(SDL_Color color)
-     */
     void renderValue();
-
-    /*
-     * Renders numbers in tiles by arbitrary
-     * color specified by `color` argument
-     */
     void renderValue(SDL_Color color);
-
-    /*
-     * Returns true, if the puzzle is completed,
-     * otherwise returns false
-     */
     bool isGameOver();
 
     /*
-     * Returns true, if current array `tiles`
-     * contains solvable configuration of tiles,
      * source: www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
      */
     bool isSolvable();
-
-    /*
-     * Shuffles `tiles` array (swaps random tiles)
-     */
     void shuffle();
-
-    /*
-     * Restarts state of game:
-     * sets `gameOver` and `endGame` to false,
-     * shuffles tiles with puzzle::shuffle(),
-     * and calls puzzle::render() to render game
-     */
     void restart();
-
-    /*
-     * Searches for empty tile. Returns `Point`
-     * structure, which contains x (column)
-     * and y (row) array coordinates in `tiles` array
-     */
     Point getEmptyTile();
-
-    /*
-     * Writes a formatted string to stdout (just like printf)
-     * Shows a message box on Windows
-     */
     void printFormatError(const char* format_string, ...);
 
 public:
     Puzzle(const char* font);
     ~Puzzle();
-
-    /*
-     * Prepends the path where the executable was run from to the
-     * string pointed by `filename`
-     */
     const char* getPath(const char* filename);
-
-    /*
-     * `renderer` setter
-     */
     void setRenderer(SDL_Renderer* renderer);
-
-    /* Draws black background, lines are drawn with white
-     * calls `renderValue(color)` with white color as an argument
-     * (renders numbers)
-     * So to sum it up, renders background, lines and numbers
-     */
     void render();
-
-    /* Same as `render()`, but renders lines and numbers by
-     * color specified by 1st argument `color`
-     */
     void render(SDL_Color color);
     void click(int x, int y);
     void keydown(SDL_Keycode key);
